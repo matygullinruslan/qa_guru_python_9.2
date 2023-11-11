7954010887516208cbfcbe9dd0c28f4419fe09c7
Руслан <150294032+matygullinruslan@users.noreply.github.com>
2023-11-11 20:48:52 +0300

    Update test_google.py
    Редактировал скрипт

--- test_google.py
+++ test_google.py
@@ -1,2 +1,16 @@
 import pytest
 from selene import browser, have
+
+
+@pytest.fixture
+def browser_open():
+    browser.config.window_width = 900
+    browser.config.window_height = 700
+
+
+def test_browser_open(browser_open):
+    print('Открывается браузер с заданными размерами')
+    browser.open('https://www.google.ru/')
+    browser.element('#APjFqb').click().type('asdfghjsdfgqasdfgujmyhnt5gbrf').press_enter()
+    browser.element('#appbar').should(have.text('Результатов: примерно 0'))
+    print('Проверяем, что по запросу ничего не найдено')
